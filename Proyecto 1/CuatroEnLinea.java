f72f09f582ec94eb60b5be2363206683e254ffda
BryanMr3 <bryanje12674@gmail.com>
2024-10-14 10:29:55 -0600

    Metodos agregados, clase CuatroEnLinea

--- Proyecto 1/CuatroEnLinea.java
+++ Proyecto 1/CuatroEnLinea.java
@@ -3,6 +3,7 @@
  * 
  */
 
+import java.util.Scanner;
 public class CuatroEnLinea
 {
     private int[][] tablero;
@@ -13,10 +14,12 @@ public class CuatroEnLinea
         this.tablero = tablero;
         this.jugadorActual = jugadorActual;
     }
-
+    
+    //metodo constructos y sin parametros. llama al metodo iniciarJuego, que inicializaba el tablero y establecia el jugador actual
     public CuatroEnLinea() {
         iniciarJuego();
     }
+    
     //setters
     public void setTablero (int[][] tablero){
         this.tablero = tablero;
@@ -46,7 +49,6 @@ public class CuatroEnLinea
             for(int j = 0; j < tablero[i].length ; j++){
                 tablero[i][j] = 0; //representa los espacios disponibles para ficha, con el valor cero
             }
-            System.out.println(); //dar estrtuctura al tablero
         }
 
         this.jugadorActual = 1; //establece al jugador actual, como  1, que representa al jugador 1
@@ -73,20 +75,81 @@ public class CuatroEnLinea
         return false;//el indice está fuera del rango de las columnas
     }
 
+    /*Este metodo imprime el tablero, se utilizan dos bucles for para recorrer el array, el indice i representa las filas, el indice j 
+     * representa las columnas. El print imprimirá cada fila y columna, de manera que se muestre el tablero completo en un inicio con los
+     * valores del metodo iniciarJuego, pero cambiará segun los jugadores vayan llenando el tablero y se llame a este metodo. 
+     */
+    public void mostrarTablero(){
+        System.out.println("Tablero actual: ");
+        for(int i=0; i < tablero.length; i++){
+            System.out.print("| "); //separar casillas
+            for(int j=0; j < tablero[i].length;j++){
+                System.out.print(tablero[i][j] + " | ");
+
+            }
+            System.out.println(); //dar espacio entra columnas
+        }
+        System.out.println();//espacio entre filas
+    }
+
+    /*metodo tipo booleano, para verificar si el juego termina. llama a dos metodos esGanador() y esEmpate() revisa con el condicional if
+     * si se cumple alguno de los dos y retorna "true", si no, retorna "false" que indica que el juego deberia continuar.
+     */
+    public boolean esJuegoTerminado(){
+        
+        if(esGanador() || esEmpate()== true){
+            return true;
+        }else{
+            return false;
+        }
+    
+    }
+
+    public boolean esGanador(){
+        
+    }
+
+    public boolean esEmpate(){
+
+    }
+
     /*metodo sin retorno y parametros, se espera alternar al jugador actual, cuando se llame al metodo se espera cambie de jugador, por medio
      * del condicional que, en if verifica si el jugador actual es 1, lo cambia a 2 y viceversa mediante el else 
      */
     public void cambiarJugador(){
-        
+
         if(this.jugadorActual==1){
             this.jugadorActual = 2;
         }else{
             this.jugadorActual = 1;
         }
+
+    }
+
+    public static void main(String[]args){
+        //Esto es solo para probar los avances, pero más adelante se dedica una clase al flujo del juego
         
-    }
+        Scanner scanner = new Scanner(System.in);
+        CuatroEnLinea juego = new CuatroEnLinea();
+        int columna;
 
-    public void mostarTablero(){
-    }
+        //interfaz
+        System.out.println("¡Bienvenido al juego!");
+        while(true){
 
+            System.out.println("Jugador " + juego.getJugadorActual() + ", es su turno. Seleccione una columna (0-6) para agregar su ficha:" );
+            juego.mostrarTablero();
+            columna = scanner.nextInt();
+
+            if(juego.hacerMovimiento(columna)){
+                System.out.println("¡Movimiento realizado!");
+                juego.mostrarTablero();
+            }else{
+                System.out.println("¡Movimiento inválido!");
+                juego.mostrarTablero();
+            }
+            System.out.println();
+        }
+
+    }
 }
