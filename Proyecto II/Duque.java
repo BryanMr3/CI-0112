ad9d4ca4da6a91c7730cccb5af0abce738ade371
JackNavarroVenegas <jacknavarrovenegas@gmail.com>
2024-11-09 20:40:29 -0600

    Primer commit del Queue del Proyecto 2
    Aquí se crearon las clases Duque, Node y Queue

---
+++ Proyecto II/Duque.java
@@ -0,0 +1,13 @@
+public class Duque{
+
+    private Nodo first;
+    private Nodo last;
+    private int large;
+
+    public Duque(){
+        first = null;
+        last = null;
+        large = 0;
+    }
+
+}
---
+++ Proyecto II/Queue.java
@@ -0,0 +1,12 @@
+public class Queue{
+
+    private Nodo first;
+    private Nodo last;
+    private int large;
+
+    public Queue(){
+        first = null;
+        last = null;
+        large = 0;
+    }
+}
