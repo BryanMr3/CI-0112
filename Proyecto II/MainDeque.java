b5c11975b9f388107e38c59b072e2938f375498e
JackNavarroVenegas <jacknavarrovenegas@gmail.com>
2024-11-16 18:36:47 -0600

    Update MainDeque.java
    Modificación del main con el método de búsqueda

--- Proyecto II/MainDeque.java
+++ Proyecto II/MainDeque.java
@@ -7,29 +7,41 @@ public class MainDeque{
         Scanner scanner = new Scanner(System.in);
         Deque colaDoble = new Deque(); 
         
-        //Bucle para solicitar dato, encolarlo al frente(de momento, ya que faltan otros metodos), y mostrar la cola actual.
-        while (true) {
-            System.out.println("\n" + "En que extremo desea agregar el elemento? 1: Frente / 2: Final");
-            int position = scanner.nextInt();
+         for(int i = 0; i<5; i++){
+         //Bucle para solicitar 5 datos, encolarlos al frente(de momento, ya que faltan otros metodos), y mostrar la cola actual.
+      
+         System.out.println("\n" + "En que extremo desea agregar el elemento? 1: Frente / 2: Final");
+         int position = scanner.nextInt();
 
-            if(position == 1){
-                System.out.println("Ingrese el valor que quiere agregar al frente");
-                int data = scanner.nextInt();
-                colaDoble.pushFront(data);
-                System.out.println("Cola actual:");
-                
-            }if (position == 2) {
-                System.out.println("Ingrese el valor que quiere agregar al final");
-                int data = scanner.nextInt();
-                colaDoble.pushBack(data);
-                System.out.println("Cola actual:");
-                
-            }
-            
-            colaDoble.printDeque();
+         if(position == 1){
+             System.out.println("Ingrese el valor que quiere agregar al frente");
+             int data = scanner.nextInt();
+             colaDoble.pushFront(data);
+             System.out.println("Cola actual:");
+             
+         }
+         if (position == 2) {
+             System.out.println("Ingrese el valor que quiere agregar al final");
+             int data = scanner.nextInt();
+             colaDoble.pushBack(data);
+             System.out.println("Cola actual:");
+             
+         }
+         
+         colaDoble.printDeque();
+       }
 
+         //Buscar elemento en el Deque
 
-            //En un futuro se puede implementar una manera de detener el programa
+        System.out.println("¿Cuál elemento desea buscar?");
+        int missing = scanner.nextInt();
+
+        if(colaDoble.searchDuque(missing) == true){
+            System.out.println("El elemento se encuentra en la cola");
+        }
+
+        if(colaDoble.searchDuque(missing) == false){
+            System.out.println("El elemento no se encuentra en la cola");
         }
     }
 }
