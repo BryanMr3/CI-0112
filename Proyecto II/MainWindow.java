791f896c78d9c515e9c020e048cb09f539660832
BryanMr3 <bryanje12674@gmail.com>
2024-11-28 22:24:50 -0600

    Correción a botón pushFront y error linea 85

--- Proyecto II/MainWindow.java
+++ Proyecto II/MainWindow.java
@@ -1,8 +1,7 @@
 
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
+import java.awt.event.*;
 import javax.swing.*;
 
 /**
@@ -21,40 +20,42 @@ public class MainWindow extends JFrame {
      */
     public MainWindow(){
         deque = new Deque();
-        setTitle("Cola doble");//titulo de la ventana
-        setSize(700, 600); //Establece el tamaño de ventana
-        setLocationRelativeTo(null); //Centra la ventana
+        setTitle("Cola doble");//Titulo de la ventana
+        setSize(700, 600); //Tamaño de ventana
+        setLocationRelativeTo(null); //Centrar la ventana
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Cerrar programa al dar "X"
         
         panelDeque = new JPanel();//inicializar un panel.
-        panelDeque.setLayout(new BoxLayout(panelDeque, BoxLayout.Y_AXIS));
+        panelDeque.setLayout(new BoxLayout(panelDeque, BoxLayout.Y_AXIS)); //Definir como se colocaran los componentes visualmente
 
-        
-        addComponents(); //llamar método que crea componentes
-        createButtons();
+        //llamado de métodos
+        addComponents();
+        createButtons(); 
     }
     
     /**
-     * Este método contiene elementos agregados
+     * Este método contiene la estructura de componentes para solicitar, agregar elementos de la cola doble
      */
     private void addComponents(){
-        //agregar etiquetas
-        JLabel dequeLabel = new JLabel("Estado actual de la cola doble:");
         
-        showDequeArea = new JTextArea();
-        showDequeArea.setEditable(false); //detener que se pueda agregar algo en el dequeShow, ya que se usará para mostrar la cola
-        JScrollPane scrollPane = new JScrollPane(showDequeArea);
+        //Componentes para mostrar la Deque
+        JLabel dequeLabel = new JLabel("Estado actual de la cola doble:"); //Etiqueta para la cola doble
+        showDequeArea = new JTextArea(10, 20); //Area de texto que permite mostrar las acciones referente a los nodos(añadir,eliminar)
+        showDequeArea.setEditable(false); //No permite editar el cuadro por el usuario
+        JScrollPane scrollPane = new JScrollPane(showDequeArea); //Si la Deque es muy extensa, permite scrolear la cola para ver todos los nodos
 
-        JLabel addLabel = new JLabel("Ingrese el valor que desea agregar:"); 
-        JLabel searchLabel = new JLabel("Ingrese el valor que desea buscar");
-                
-        //agregar cuadro de texto
-        valueField = new JTextField(20);
-        searchField = new JTextField(20);
+        //Componentes para solicitar número
+        JLabel addLabel = new JLabel("Ingrese el valor que desea agregar:"); //Etiqueta para solicitar un número para agregar 
+        valueField = new JTextField(20); //Cuadro de texto para agregar el número
+
+
+        //Componentes para buscar número
+        JLabel searchLabel = new JLabel("Ingrese el valor que desea buscar"); //Etiqueta para solicitar un número para buscar
+        searchField = new JTextField(20); //Cuadro de texto para agregar el número
     
+        
         //añadir componentes al panel
-        panelDeque.add(dequeLabel);
-        panelDeque.add(showDequeArea);
+        panelDeque.add(dequeLabel); 
         panelDeque.add(scrollPane);
         panelDeque.add(addLabel);
         panelDeque.add(valueField);
@@ -63,25 +64,31 @@ public class MainWindow extends JFrame {
         add(panelDeque);//agregar panel al JFrame
     }
 
+    //Método para darle función a los botones y maneja errores
     private void createButtons(){
        
         //agregar botones
         JButton searchbButton = new JButton("Buscar");
          
         JButton pushFrontButton = new JButton("Encolar al frente"); //botón para encolar al frente un valor.
-        pushFrontButton.addActionListener(e->{
-            String text = valueField.getText();//obtener el texto del cuadro de texto   
-            
-            //manejo de excepcion, si al colocar un dato diferente de un entero muestra un mensaje de que no es un numero, que ingrese un numero
-            try {
-                int value = Integer.parseInt(text); //convertir este texto a entero
-                deque.pushFront(value);
-                showDequeArea.setText(deque.printDeque()); //agregar al area de texto el número, pero lo convierte a String
-                
-            } catch (Exception ex) {
-                JOptionPane.showMessageDialog(pushFrontButton, "Esto no es un número, Ingrese un número", "pene", JOptionPane.ERROR_MESSAGE);
-            }
-        });
+                pushFrontButton.addActionListener(new ActionListener(){
+                    public void actionPerformed(ActionEvent e){
+                        String text = valueField.getText();//obtener el texto del cuadro de texto   
+                        
+                        try {
+                            int value = Integer.parseInt(text);
+                            deque.pushFront(value);
+                            showDequeArea.setText(deque.printDeque());
+                        } catch (Exception ex) {
+                            JOptionPane.showMessageDialog(pushFrontButton, "Esto no es un número entero, ingrese un número entero", "Error", JOptionPane.ERROR_MESSAGE);
+                        }
+                    }
+                });
+                pushFrontButton.setBounds(158,109,97,25);
+        
+
+
+        
         //AUN NO FUNCIONA BIEN, AL AGREGAR EL NUMERO SE MUEVE TODO(CUADROS DE TEXTO, BOTONES, VERIFICAR ESTE ERROR)
         //VERIFICAR SI EL MANEJO DE EXCEPCIONES ESTA COMPLETO PARA ESTE BOTON
         //EL E DE ACTION LISTENER MUESTRA MENSAJE QUE NO SE ESTÁ USANDO, VERIFICAR EL POR QUÉ
